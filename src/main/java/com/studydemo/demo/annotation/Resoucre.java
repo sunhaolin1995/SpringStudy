820e31851372a14c3fbb357c80e8cd35e667a73e
HaoLin.Sun <sunhaolin@315che.com>
2022-07-08 13:46:26 +0800

    Initial commit

---
+++ src/main/java/com/studydemo/demo/annotation/Resoucre.java
@@ -0,0 +1,8 @@
+package com.studydemo.demo.annotation;
+
+/**
+ * @Author:SunHaoLin
+ * @Create:2022/7/8 13:39
+ */
+public class Resoucre {
+}
---
+++ src/test/java/com/studydemo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studydemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
